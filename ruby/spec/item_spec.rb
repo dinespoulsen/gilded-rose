7b12d6dfe0d7eee15e9ba8a5500ab80f16a79ea6
Dines Poulsen <dinespoulsen84@gmail.com>
2017-01-24 13:35:43 +0000

    Written tests for the properties of an item

--- ruby/spec/item_spec.rb
+++ ruby/spec/item_spec.rb
@@ -8,5 +8,15 @@ describe Item do
       cheese = Item.new("Cheese", 10, 10)
       expect(cheese.name).to eq("Cheese")
     end
+
+    it "comes with a sell in property" do
+      cheese = Item.new("Cheese", 10, 10)
+      expect(cheese.sell_in).to eq(10)
+    end
+
+    it "comes with a quality property" do
+      cheese = Item.new("Cheese", 10, 10)
+      expect(cheese.quality).to eq(10)
+    end
   end
 end
